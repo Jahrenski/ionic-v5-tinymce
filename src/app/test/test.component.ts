aa9ad53ba081059a0d34723ab24e8a7dda7ad3c0
Richard <savoier@Plurilogic.com>
2020-03-03 16:17:34 -0500

    fixed the default project for ionic 5 / angular 9

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector:    'app-test',
   templateUrl: './test.component.html',
-  styles: [':host{ display: block; overflow: auto; min-height: 100%;}']
+  styles: [':host{ overflow: auto; min-height: 100%;}']
 })
 export class TestComponent {
   
